2bd5a5e1c57f2c90304d92bc7d609f6aeb575c53
Paul Pavlovsky <fatpaher@gmail.com>
2019-04-24 16:32:09 +0300

    Extract request interval as constant

--- lib/server.rb
+++ lib/server.rb
@@ -1,11 +1,13 @@
 class Server
+ REQUEST_INTERVAL = 60
+
  def self.run
   last_ran_at = Time.now
   request_data = StatusTracker.new.call
 
   loop do
    run_time = Time.now
-   next if (run_time - last_ran_at).to_i < 60
+   next if (run_time - last_ran_at).to_i < REQUEST_INTERVAL
 
    handler = StatusTracker.new(previous_data: request_data)
    handler.call
