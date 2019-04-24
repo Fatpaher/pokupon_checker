0509b7b65e71863e0dee75dcff2f4e7c1aff0b07
Paul Pavlovsky <fatpaher@gmail.com>
2019-04-24 17:18:13 +0300

    Add default mail server configuration

--- config/init.rb
+++ config/init.rb
@@ -3,3 +3,7 @@ require_relative '../lib/status_request'
 require_relative '../lib/report'
 require_relative '../lib/server'
 
+Mail.defaults do
+  delivery_method :smtp, address: "localhost", port: 2500
+end
+
